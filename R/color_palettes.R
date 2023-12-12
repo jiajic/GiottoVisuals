8027dad5b2475b5335589dc6d9c00cd8b1794437
jiajic <72078254+jiajic@users.noreply.github.com>
2023-12-12 12:02:48 -0500

    Remove `getDistinctColors`

--- R/color_palettes.R
+++ R/color_palettes.R
@@ -115,55 +115,6 @@ getColors <- function(pal = 'viridis',
 
 
 
-#' @title getDistinctColors
-#' @description Returns a number of distinct colors based on the RGB scale
-#' @param n number of colors wanted
-#' @return character vector of hexadecimal distinct colors
-#' @export
-getDistinctColors <- function(n) {
-
-  if(n < 1) stop(wrap_txt('getDistinctColors \'n\' colors wanted must be at least 1\n', errWidth = TRUE), call. = FALSE)
-
-  qual_col_pals <- RColorBrewer::brewer.pal.info[RColorBrewer::brewer.pal.info$category == 'qual',]
-  col_vector <- unique(unlist(mapply(RColorBrewer::brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals))));
-
-  if(n > length(col_vector)) {
-
-    # get all possible colors
-    all_colors = grDevices::colors()
-    all_colors_no_grey = grep(x = all_colors, pattern = 'grey|gray', value = T, invert = T)
-    grey_colors = grep(x = all_colors, pattern = 'grey', value = T, invert = F)
-    admitted_grey_colors = grey_colors[seq(1, 110, 10)]
-    broad_colors = c(all_colors_no_grey, admitted_grey_colors)
-
-    set.seed(1234)
-    on.exit(set.seed(Sys.time()))
-    # if too many colors requested, warn about recycling
-    if(n > length(broad_colors)) {
-      warning('\n not enough unique colors in R, maximum = 444 \n')
-      col_vector = sample(x = broad_colors, size = n, replace = TRUE)
-    } else {
-      col_vector = sample(x = broad_colors, size = n, replace = FALSE)
-    }
-
-  } else {
-
-    xxx <- grDevices::col2rgb(col_vector);
-    dist_mat <- as.matrix(stats::dist(t(xxx)));
-    diag(dist_mat) <- 1e10;
-    while (length(col_vector) > n) {
-      minv <- apply(dist_mat,1,function(x)min(x));
-      idx <- which(minv==min(minv))[1];
-      dist_mat <- dist_mat[-idx, -idx];
-      col_vector <- col_vector[-idx]
-    }
-
-  }
-  return(col_vector)
-}
-
-
-
 
 
 # get palettes ####
