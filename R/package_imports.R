e366fa885233e598ad7d8a7733fc7a8299621ad8
George Chen <72078254+jiajic@users.noreply.github.com>
2024-11-14 17:19:42 -0500

    chore: add setNames() from stats

--- R/package_imports.R
+++ R/package_imports.R
@@ -1,7 +1,7 @@
 # data.table class import needed for DT dispatch to work properly
 #' @importClassesFrom data.table data.table
 #' @importFrom data.table dcast dcast.data.table
-#' @importFrom stats cov var
+#' @importFrom stats cov var setNames
 #' @importFrom methods slot new
 #' @importFrom methods setGeneric setMethod
 #' @importFrom colorRamp2 colorRamp2
